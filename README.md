ae579cf90972187bf96e45a318448c29cf1cd1ff
Rahim <benmelouka@live.fr>
2022-12-05 23:14:20 +0100

    V3 + ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,143 @@
-"# baby-names" 
+| Université Paris-Saclay |
+| --- |
+|
+# Nom des bébés dans les Etats-Unis de 1880 à 2015
+
+Module : Visualisation de données
+
+Auteur : Abderrahim Benmelouka
+
+
+# Table des matières
+
+[1. Introduction](#_Toc121173419)
+
+[2. Données brutes](#_Toc121173420)
+
+[3. Transformation des données](#_Toc121173421)
+
+[3.1. Jeu de données « Total Births »](#_Toc121173425)
+
+[3.2. Jeu de données « Unique Names »](#_Toc121173426)
+
+[3.3. Jeu de données « Name Records »](#_Toc121173427)
+
+[3.4. Jeu de données « Unisex Names »](#_Toc121173428)
+
+[4. Tableau de bord](#_Toc121173429)
+
+[4.1. Total Births](#_Toc121173431)
+
+[4.2. Unique Names](#_Toc121173432)
+
+[4.3. Most Popular Names](#_Toc121173433)
+
+[4.4. Unisex Names](#_Toc121173434)
+
+[4.5. Name Analysis](#_Toc121173435)
+
+[4.6. Name Ranking](#_Toc121173436)
+
+[5. Conclusion](#_Toc121173437)
+
+# 1.Introduction
+
+Ce projet consiste à prendre un ensemble de données et à en tirer un aperçu à travers différentes visualisations. Il se décompose en deux grandes parties : pré-traiter les données brutes et développer le tableau de bord de visualisation.
+
+Le tableau de bord a été créé à l'aide de Dash, un framework qui permet de créer des applications de données en Python pur (pas de JavaScript requis). Dash utilise Plotly sous le capot pour dessiner ses graphiques. Plus précisément, j'ai utilisé Plotly Express car il simplifie énormément le processus de création de figures.
+
+Le code source de l'ensemble du projet se trouve sur [mon repo GitHub](https://github.com/Abdou27/baby-names). Pour lancer le serveur, clonez le repo dans un dossier local puis exécutez la commande ```python main.py```.
+
+# 2.Données brutes
+
+Le jeu de données utilisé s'appelle babyNamesUSYOB-full, il comporte 4 colonnes et 1858689 lignes. Ci-dessous les 5 premières lignes et 5 dernières lignes :
+
+| **YearOfBirth** | **Name** | **Sex** | **Number** |
+| --- | --- | --- | --- |
+| **1880** | Mary | F | 7065 |
+| **1880** | Anna | F | 2604 |
+| **1880** | Emma | F | 2003 |
+| **1880** | Elizabeth | F | 1939 |
+| **1880** | Minnie | F | 1746 |
+| … | … | … | … |
+| **2015** | Zykell | M | 5 |
+| **2015** | Zyking | M | 5 |
+| **2015** | Zykir | M | 5 |
+| **2015** | Zyrus | M | 5 |
+| **2015** | Zyus | M | 5 |
+
+# 3.Transformation des données
+
+À l'aide du notebook dashboard.ipynb trouvé dans le dossier notebooks. Quatre nouveaux jeux de données ont été extraits en ajoutant des colonnes calculées et en agrégeant, filtrant et triant les données.
+
+## 3.1. Jeu de données « Total Births »
+
+Cet ensemble de données décrit le nombre de naissances masculines, féminines et totales chaque année, il a été créé en regroupant les données d'origine par YearOfBirth et Sex, puis en additionnant le nombre de naissances pour chaque groupe.
+
+Avec ce DataFrame résultant, nous regroupons par YearOfBirth et ajoutons une troisième ligne qui additionne les naissances masculines et féminines pour chaque groupe, cette ligne représente le total des naissances par an sans distinction de sexe.
+
+Ensuite, les résultats sont enregistrés dans data/total\_births.csv.
+
+## 3.2. Jeu de données « Unique Names »
+
+Cet ensemble de données décrit le nombre de noms uniques masculins, féminins et totaux chaque année, il a été créé en regroupant les données d'origine par YearOfBirth et Sex, puis en comptant le nombre de nombre de noms différents pour chaque groupe.
+
+Pour calculer le nombre de prénoms uniques par an quel que soit le sexe, une simple addition des lignes masculines et féminines serait une erreur car il faudrait tenir compte des prénoms unisexes. Par conséquent, pour y parvenir, les colonnes Sex et Number ont été supprimées, puis les lignes en double ont été supprimées dans le DataFrame résultant, cela élimine les doublons dans les noms unisexes, après cela, le résultat est regroupé par année, puis le nombre de noms différents par an est compté.
+
+Il ne reste plus qu'à ajouter les deux DataFrames et à renommer le colonne Name en Count.
+
+Ensuite, les résultats sont enregistrés dans data/unique\_names.csv.
+
+## 3.3. Jeu de données « Name Records »
+
+Ce jeu de données est une version augmentée des données d'origine, il ajoute trois nouvelles colonnes calculées.
+
+La première colonne est la proportion d'utilisation de chaque nom par an et par sexe. Cela a été calculée en regroupant par année et par sexe, puis en divisant chaque nombre par la somme des nombres des groupes résultants.
+
+La deuxième colonne est la longueur du nom, cela a été facilement calculé en appliquant la fonction len de Python à chaque ligne du DataFrame.
+
+La troisième colonne décrit la décennie à laquelle appartient l'année, cela a également été facilement calculé en divisant l'année de chaque ligne par 10, puis en prenant la partie entière de ce résultat et en la multipliant par 10.
+
+Ce DataFrame est ensuite enregistré dans data/name\_records.csv.
+
+## 3.4. Jeu de données « Unisex Names »
+
+Ce jeu de données est un classement décroissant de tous les prénoms unisexes, c'est-à-dire des prénoms ayant été utilisés au moins une fois pour les deux sexes.
+
+Le classement est basé sur le nombre minimum de fois que chaque nom a été utilisé par sexe, par exemple, le nom Willie a été utilisé au total 146 134 fois pour les bébés de sexe féminin et 448 091 fois pour les bébés de sexe masculin. Par conséquent, la valeur attribuée à ce nom est 146134. Les noms sont ensuite classés par ordre décroissant en fonction des valeurs qui leur sont attribuées. Le nom Willie est le premier dans le DataFrame résultant.
+
+Le résultat est enregistré dans data/unisex\_names.csv.
+
+# 4.Tableau de bord
+
+Le tableau de bord est organisé en différents onglets (avec Dash) pour faciliter l'accès et la commutation entre les différents graphiques.
+
+## 4.1. Total Births
+
+Ce graphique linéaire montre l'évolution du nombre de naissances au fil des ans pour chaque sexe.
+
+## 4.2. Unique Names
+
+Ce graphique, également un Line Chart, montre comment le nombre de noms uniques a changé au fil du temps pour chaque sexe.
+
+## 4.3. Most Popular Names
+
+Ce Treemap montre les noms les plus populaires qui ont jamais été choisis, il y a un curseur pour changer dynamiquement le nombre de noms à afficher.
+
+## 4.4. Unisex Names
+
+Cette arborescence montre les noms unisexes les plus utilisés, il y a aussi un curseur pour changer dynamiquement le nombre de noms à afficher.
+
+## 4.5. Name Analysis
+
+Ces deux graphiques linéaires montrent l'utilisation et la proportion (popularité) d'un nom donné. Le champ de texte est utilisé pour changer le nom pour lequel afficher les graphiques. Comme indiqué, on peut aussi sélectionner un nom en cliquant dessus dans les deux graphes Treemaps précédents.
+
+## 4.6. Name Ranking
+
+Ce graphique de classement montre l'évolution du classement des noms au fil des décennies. Il y a un curseur pour sélectionner le nombre de noms principaux à afficher.
+
+Il est à noter qu'en raison de l'indisponibilité d'un graphique de classement dans Plotly, ce graphique a été créé manuellement en superposant un nuage de points et un graphique linéaire.
+
+# 5.Conclusion
+
+Ce projet m'a aidé à développer mes compétences d'analyse de données à travers l'étape de prétraitement et mes compétences de visualisation à travers le processus de création de tableau de bord avec Dash.
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
+import colorsys
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -18,23 +19,49 @@ COLORS = {
 }
 
 
+def hsv_to_rgb(h, s, v):
+    (r, g, b) = colorsys.hsv_to_rgb(h, s, v)
+    return int(255 * r), int(255 * g), int(255 * b)
+
+
+def rgb_to_hex(r, g, b):
+    return '#{:02X}{:02X}{:02X}'.format(r, g, b)
+
+
+def get_distinct_colors(n):
+    hue_partition = 1.0 / (n + 1)
+    return list(rgb_to_hex(*hsv_to_rgb(hue_partition * value, 1.0, 1.0)) for value in range(0, n))
+
+
 def total_births_per_year_fig():
-    return px.line(total_births, x="YearOfBirth", y="Number", color="Sex", color_discrete_map=COLORS)
+    fig = px.line(total_births, x="YearOfBirth", y="Number", color="Sex", color_discrete_map=COLORS)
+    fig.update_layout(
+        title="Number of births per year",
+        xaxis_title="Year of birth",
+        yaxis_title="Number of births",
+    )
+    return fig
 
 
 def unique_names_per_year_fig():
-    return px.line(unique_names, x="YearOfBirth", y="Count", color="Sex", color_discrete_map=COLORS)
+    fig = px.line(unique_names, x="YearOfBirth", y="Count", color="Sex", color_discrete_map=COLORS)
+    fig.update_layout(
+        title="Number of unique names per year",
+        xaxis_title="Year of birth",
+        yaxis_title="Number of unique names",
+    )
+    return fig
 
 
 @app.callback(
-    Output('treemap_fig', 'figure'),
+    Output('most_popular_names_fig', 'figure'),
     Input('name_treemap_slider', 'value'))
-def update_treemap_fig(value):
+def update_most_popular_names_fig(value):
     if not value:
         value = 50
     most_used_names = name_records.groupby('Name')['Number'].sum().reset_index() \
                           .sort_values(by=["Number"], ascending=False).reset_index(drop=True)[:value]
-    most_used_names["Rank"] = [i+1 for i in range(most_used_names.shape[0])]
+    most_used_names["Rank"] = [i + 1 for i in range(most_used_names.shape[0])]
     fig = px.treemap(
         data_frame=most_used_names,
         names="Name",
@@ -57,7 +84,7 @@ def update_unisex_names_fig(value):
     if not value:
         value = 50
     most_popular_unisex_names = unisex_names[:value]
-    most_popular_unisex_names["Rank"] = [i+1 for i in range(most_popular_unisex_names.shape[0])]
+    most_popular_unisex_names["Rank"] = [i + 1 for i in range(most_popular_unisex_names.shape[0])]
     fig = px.treemap(
         data_frame=most_popular_unisex_names,
         names="Name",
@@ -78,7 +105,13 @@ def update_unisex_names_fig(value):
     Input('selected_name', 'value'))
 def update_name_usage_fig(selected_name):
     selected_name_data = name_records[name_records["Name"] == selected_name]
-    return px.line(selected_name_data, x="YearOfBirth", y="Number", color="Sex", color_discrete_map=COLORS)
+    fig = px.line(selected_name_data, x="YearOfBirth", y="Number", color="Sex", color_discrete_map=COLORS)
+    fig.update_layout(
+        title="Usage of the name %s per year" % selected_name,
+        xaxis_title="Year of birth",
+        yaxis_title="Usage",
+    )
+    return fig
 
 
 @app.callback(
@@ -86,17 +119,23 @@ def update_name_usage_fig(selected_name):
     Input('selected_name', 'value'))
 def update_name_proportion_fig(selected_name):
     selected_name_data = name_records[name_records["Name"] == selected_name]
-    return px.line(selected_name_data, x="YearOfBirth", y="Proportion", color="Sex", color_discrete_map=COLORS)
+    fig = px.line(selected_name_data, x="YearOfBirth", y="Proportion", color="Sex", color_discrete_map=COLORS)
+    fig.update_layout(
+        title="Proportion of the name %s per year" % selected_name,
+        xaxis_title="Year of birth",
+        yaxis_title="Proportion",
+    )
+    return fig
 
 
 @app.callback(
     Output('selected_name', 'value'),
-    Input('treemap_fig', 'clickData'),
+    Input('most_popular_names_fig', 'clickData'),
     Input('unisex_names_fig', 'clickData'))
 def update_selected_name(mpn_data, mpun_data):
     if ctx.triggered_id is None:
         return "Mary"
-    click_data = mpn_data if ctx.triggered_id == "treemap_fig" else mpun_data
+    click_data = mpn_data if ctx.triggered_id == "most_popular_names_fig" else mpun_data
     return click_data["points"][0]["label"]
 
 
@@ -132,16 +171,30 @@ def update_name_rankings_fig(value):
 
     name_rankings = name_rankings.groupby("Name").apply(add_missing_ranks).reset_index(drop=True).sort_values(
         by=["Decade"]).reset_index(drop=True)
-    fig1 = px.scatter(name_rankings, x="Decade", y="Rank", color="Name")
-    fig2 = px.line(name_rankings, x="Decade", y="Rank", color="Name", hover_name="Name")
+    names = name_rankings["Name"].unique()
+    colors = list(get_distinct_colors(len(names)))
+    colors = dict(zip(names, colors))
+    print(colors)
+    fig1 = px.scatter(name_rankings, x="Decade", y="Rank", color="Name",
+                      color_discrete_map=colors)
+    fig2 = px.line(name_rankings, x="Decade", y="Rank", color="Name", hover_name="Name",
+                   color_discrete_map=colors)
     fig2.update_traces(line={"width": 10}, opacity=0.5)
     fig = go.Figure(data=fig1.data + fig2.data)
     fig['layout']['yaxis']['range'] = (value + 1, 0)
+    fig.update_layout(
+        title="Top %d names per decade" % value,
+        xaxis_title="Decade",
+        yaxis_title="Rank",
+    )
     return fig
 
 
 app.layout = html.Div(children=[
-    html.H1(children='Data Visualisation : Baby Names in the USA from 1880 to 2015', style={"margin-left": "10px"}),
+    html.Div(children=[
+        html.H1(children='Data Visualisation : Baby Names in the USA from 1880 to 2015',
+                style={"margin-left": "10px", "color": "white", "margin-bottom": "0", "padding": "10px"}),
+    ], style={"background": "black"}),
 
     dcc.Tabs([
         dcc.Tab(label='Total Births', children=[
@@ -150,31 +203,41 @@ app.layout = html.Div(children=[
         dcc.Tab(label='Unique Names', children=[
             dcc.Graph(figure=unique_names_per_year_fig(), style={"height": "calc(100vh - 190px)"})
         ]),
-        dcc.Tab(label='Name Analysis', children=[
-            html.Div(children=[
-                dcc.Graph(id="name_usage", style={"width": "50%"}),
-                dcc.Graph(id="name_proportion", style={"width": "50%"}),
-            ], style={"display": "flex"}),
-            html.Label(htmlFor="selected_name", children=["Selected Name :"], style={"margin-left": "100px"}),
-            dcc.Input(id='selected_name', value='Mary', type='text', style={"margin-left": "10px"}),
-        ]),
         dcc.Tab(label='Most Popular Names', children=[
+            html.Label(htmlFor="name_treemap_slider", children=["Select the number of names to display :"],
+                       style={"margin-left": "20px", "margin-top": "20px"}),
             dcc.Slider(10, 100, 5, marks={i: {"label": str(i)} for i in range(10, 101, 5)}, id='name_treemap_slider',
                        value=50),
-            dcc.Graph(id="treemap_fig", style={"height": "calc(100vh - 200px)"}),
+            dcc.Graph(id="most_popular_names_fig", style={"height": "calc(100vh - 220px)"}),
         ]),
         dcc.Tab(label='Unisex Names', children=[
+            html.Label(htmlFor="unisex_names_slider", children=["Select the number of names to display :"],
+                       style={"margin-left": "20px", "margin-top": "20px"}),
             dcc.Slider(10, 100, 5, marks={i: {"label": str(i)} for i in range(10, 101, 5)}, id='unisex_names_slider',
                        value=50),
-            dcc.Graph(id="unisex_names_fig", style={"height": "calc(100vh - 200px)"})
+            dcc.Graph(id="unisex_names_fig", style={"height": "calc(100vh - 220px)"})
+        ]),
+        dcc.Tab(label='Name Analysis', children=[
+            html.Label(htmlFor="selected_name", children=["Selected Name :"],
+                       style={"margin-left": "38px", "margin-top": "40px"}),
+            dcc.Input(id='selected_name', value='Mary', type='text', style={"margin-left": "10px"}),
+            html.Span(children=[
+                'Note : You can also select a name by clicking on it in one of the two treemaps from "Most Popular Names" and "Unisex Names"'],
+                style={"margin-left": "10px"}),
+            html.Div(children=[
+                dcc.Graph(id="name_usage", style={"width": "50%", "height": "calc(100vh - 205px)"}),
+                dcc.Graph(id="name_proportion", style={"width": "50%", "height": "calc(100vh - 205px)"}),
+            ], style={"display": "flex"}),
         ]),
         dcc.Tab(label='Name Ranking', children=[
+            html.Label(htmlFor="name_ranking_slider", children=["Select the number of top names to display :"],
+                       style={"margin-left": "20px", "margin-top": "20px"}),
             dcc.Slider(3, 10, 1, marks={i: {"label": str(i)} for i in range(3, 11, 1)}, id='name_ranking_slider',
                        value=5),
-            dcc.Graph(id="name_ranking_fig", style={"height": "calc(100vh - 165px)"}),
+            dcc.Graph(id="name_ranking_fig", style={"height": "calc(100vh - 220px)"}),
         ]),
     ]),
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
